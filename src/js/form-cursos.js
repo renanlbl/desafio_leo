bb6f5c84f5f84d53bc57b8ef89e2dc7ea2655f32
renanlblu <cursosprogramacoes@gmail.com>
2019-03-30 19:06:51 -0300

    Add course without database or storage

--- src/js/form-cursos.js
+++ src/js/form-cursos.js
@@ -1,17 +1,89 @@
 let buttonAdd = document.querySelector('.courses__add')
 let buttonClose = document.querySelector('.form__close')
-let form = document.querySelector('.form__courses')
+let formWrapper = document.querySelector('.form__courses')
+let formUsers = document.querySelector('#form__courses--add')
+let fields = document.querySelectorAll('#form__courses--add [name]')
+let course = {}
 
+onSubmit()
 openForm()
+closeForm() 
+
 function openForm () {
   buttonAdd.addEventListener('click', function() {
-    form.style.display = 'flex'
+    formWrapper.style.display = 'flex'
   })
 }
 
-closeForm() 
 function closeForm() {
   buttonClose.addEventListener('click', function() {
-    form.style.display = 'none'
+    formWrapper.style.display = 'none'
   })
 }
+
+function getPhoto() {
+  return new Promise(function(resolve, reject) {
+    let fileReader = new FileReader()
+    let userPhoto = [...formUsers.elements].filter((item) => {
+      if (item.name === 'photo') {
+        return item
+      }
+    })
+    
+    let file = userPhoto[0].files[0]
+
+    fileReader.onload = () => {
+      resolve(fileReader.result)
+    }
+
+    fileReader.onerror = () => {
+      reject(e)
+    }
+
+    fileReader.readAsDataURL(file)
+  })
+}
+
+function onSubmit () {
+  formUsers.addEventListener('submit', function(e) {
+    e.preventDefault()    
+    fields.forEach((item, index) => {
+      course[item.name] = item.value
+    })   
+
+    getPhoto().then(function(content) {
+      course['photo'] = content
+      addCourse(course)
+    }, function(e) {
+      console.error(e)
+    })
+
+    clearForm()
+  }) 
+}
+
+
+function clearForm() {
+  fields.forEach((item, index) => {
+    item.value = ''
+  })  
+  formWrapper.style.display = 'none'
+}
+
+function addCourse(dataCourse) { 
+  let divList = document.querySelector('.courses__list')
+  let div = document.createElement('div') 
+  div.classList.add('courses__card')
+  div.innerHTML = `   
+    <img src="${dataCourse.photo}" />
+    <div class="courses__card--description">
+      <h4>${dataCourse.titulo}</h4>
+      <p>${dataCourse.descricao}</p>
+    </div>
+    <a class="courses__button flexHorizontalVertical" href="#">ver curso</a>  
+  `
+
+  divList.insertBefore(div, divList.firstChild)
+}
+
+
